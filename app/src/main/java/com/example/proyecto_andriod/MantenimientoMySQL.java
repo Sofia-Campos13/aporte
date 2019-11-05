3da34fb692dfc11ddb3ba213e608e96f406bae34
sofia-campos13 <sofycampos342@gmail.com>
2019-11-05 11:22:53 -0600

    mas activitys

---
+++ app/src/main/java/com/example/proyecto_andriod/MantenimientoMySQL.java
@@ -0,0 +1,18 @@
+package com.example.proyecto_andriod;
+
+class MantenimientoMySQL {
+    public void guardar(Main2Activity main2Activity, String codigo, String descripcion, String precio) {
+    }
+
+    public void consultarCodigo(Main2Activity main2Activity, String codigo) {
+    }
+
+    public void consultarDescripcion(Main2Activity main2Activity, String descripcion) {
+    }
+
+    public void modificar(Main2Activity main2Activity, Dto datos) {
+    }
+
+    public void eliminar(Main2Activity main2Activity, String codigo) {
+    }
+}
